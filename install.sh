2fdf3271bebc3ed9b5d674785d0e28e65f2d1f40
Cassio Dias <cassio.dias@mastercard.com>
2017-09-12 16:26:09 +0100

    creating a proper link

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 unzip ./local-validator-4.34.17.zip
-cd /usr/local/bin/
-ln -s ./local-validator-4.34.17/bin/validator validator
-
-echo "DONE"
+ln -s ${PWD}/local-validator-4.34.17/bin/validator /usr/local/bin/validator
 
 validator -?
