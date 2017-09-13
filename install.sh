0c3108c6e726a97a9d85fc407dc57adfa52326bb
Cassio Dias <cassio.dias@mastercard.com>
2017-09-13 10:57:25 +0100

    installing in /Users/e055852/neustar-local-validator

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir $HOME/neustar-local-validator && cd "$_"
+
 wget http://static.wpm.neustar.biz/tools/local-validator.zip
 unzip local-validator.zip
+
 latest_name="$(ls -d local-validator-*)"
 ln -s ${PWD}/${latest_name}/bin/validator /usr/local/bin/validator
-rm -rf ./local-validator.zip
+rm -rf local-validator.zip
+
 validator -?
